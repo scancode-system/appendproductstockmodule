2f99f99fd5117289977d2abb8597c74ae624bb31
Your Name <you@example.com>
2019-12-13 18:30:23 -0300

    .

--- Config/config.php
+++ Config/config.php
@@ -1,19 +1,20 @@
 <?php
 
 return [
-    'name' => 'AppendProductStock', 
-    'fields' => ['available', 'date_delivery'],
+	'name' => 'AppendProductStock', 
+	'fields' => ['available', 'date_delivery'],
 	'sample' => [
 		[
 			'name' => 'available',
 			'observation' => 'Quantidade disponibilizada de produtos para geração de pedidos.',
+			'sample_1' => '500',
 			'filled' => false
 		], 
 		[
 			'name' => 'date_delivery',
 			'observation' => 'Data aproximada em que o produto será entregue.',
+			'sample_1' => '25/12/2020',
 			'filled' => false
 		]
 	]
 ];
-  
